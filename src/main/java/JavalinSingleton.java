22b5a182373a24727b1880078d24507b2766c88f
Saumya Puthenveettil <sputhen2000@gmail.com>
2024-10-19 18:07:39 +0000

    Completed Coding Lab: Request Body

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -22,8 +22,13 @@ public class JavalinSingleton {
          */
         app.post("/echo", ctx -> {
             
-            //implement logic here
-                
+            String jsonString=ctx.body(); 
+            Song song = om.readValue(jsonString, Song.class);
+    
+    //now we can use the 'user' response body as a Java object in whatever way we see fit.
+        
+    //eg: return the user as the response body, but also have Javalin convert it to JSON  
+    ctx.json(song);    
         });
 
         /**
@@ -35,9 +40,19 @@ public class JavalinSingleton {
          */
         app.post("/changeartisttobeatles", ctx -> {
 
-            //implement logic here
-               
-        });
+            String jsonString=ctx.body();
+
+            //utilize jackson to convert the json string to a user object
+      
+            Song song=om.readValue(jsonString,Song.class);
+    
+            //change the last name
+            song.setArtistName("Beatles");
+           
+    
+            //generate an HTTP response with the user object in the response body as a JSON.
+            ctx.json(song);
+            });
 
 
         return app;
